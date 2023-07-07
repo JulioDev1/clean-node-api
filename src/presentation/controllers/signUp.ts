37d07714ff47b6779b43647eeb2ba37d1479f3c8
Julio Cesar <juliocesargualberto617@gmail.com>
2023-07-07 12:59:17 -0300

    refactor: ensure sign up controller call emailValidator

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from "../error/invalid-param-error";
 import { MissingParamError } from "../error/missing-error";
+import { ServerError } from "../error/server-error";
 import { badRequest, internalServerError } from "../helpers/helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validtor";
@@ -7,25 +8,38 @@ import { HttpResponse, HttpRequest } from "../protocols/http";
 
 export class SignUpController implements Controller {
   private emailValidator: EmailValidator;
+
   constructor(emailValidator: EmailValidator) {
     this.emailValidator = emailValidator;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredField = ["name", "email", "password", "passwordConfirmation"];
+    try {
+      const requiredField = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
 
-    for (const field of requiredField) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredField) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+
+      return internalServerError(new Error("Internal server error"));
+    } catch (erro) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"));
-    }
-
-    return internalServerError(new Error("Internal server error"));
   }
 }
---
+++ src/presentation/error/server-error.ts
@@ -0,0 +1,5 @@
+export class ServerError extends Error {
+  constructor() {
+    super("internal server error"), this.name;
+  }
+}
