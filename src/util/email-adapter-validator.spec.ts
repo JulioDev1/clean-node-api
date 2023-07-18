90269464673794ac65b13b804b4bc0b5633fa29a
Julio Cesar <juliocesargualberto617@gmail.com>
2023-07-18 12:39:44 -0300

    test:ensure emailValidatorAdapter calls validator with correct email

--- src/util/email-adapter-validator.spec.ts
+++ src/util/email-adapter-validator.spec.ts
@@ -10,9 +10,7 @@ jest.mock("validator", () => ({
 describe("EmailValidator Adapter ", () => {
   test("Should return false if validator return false", () => {
     const sut = new EmailValidatorAdpater();
-
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
-
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
   });
@@ -21,5 +19,13 @@ describe("EmailValidator Adapter ", () => {
     const sut = new EmailValidatorAdpater();
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(true);
+  });
+
+  test("Should return true if validator return false", () => {
+    const sut = new EmailValidatorAdpater();
+    const isEmailSpy = jest.spyOn(validator, "isEmail");
+    sut.isValid("any_email@mail.com");
+
+    expect(isEmailSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
