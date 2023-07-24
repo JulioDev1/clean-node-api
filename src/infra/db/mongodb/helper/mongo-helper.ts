436585359a62104c0a43a3e14dfe4aef771e172e
Julio Cesar <juliocesargualberto617@gmail.com>
2023-07-24 15:54:13 -0300

    refactor:move mongodb to a helper method

---
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,13 @@
+import { MongoClient } from "mongodb";
+export const MongoHelper = {
+  client: MongoClient,
+  async connect(uri: string): Promise<void> {
+    this.client = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  },
+  async disconnect(): Promise<void> {
+    await this.client.close();
+  },
+};
