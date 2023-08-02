d45373cce6228e263ccb09a0adf9e43739109076
Julio Cesar <juliocesargualberto617@gmail.com>
2023-08-02 10:50:14 -0300

    test:ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,8 +1,13 @@
 import { MongoHelper } from "../helper/mongo-helper";
+import { AccountMongoRepository } from "./account";
 
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
-    await MongoHelper.connect(global.__MONGO_URI__);
+    if (!process.env.MONGO_URL) {
+      throw new Error("enviroment ambient is not defined");
+    }
+
+    await MongoHelper.connect(process.env.MONGO_URL);
   });
   afterAll(async () => {
     await MongoHelper.disconnect();
@@ -10,11 +15,12 @@ describe("Account Mongo Repository", () => {
 
   test("Should return an account on sucess", async () => {
     const sut = new AccountMongoRepository();
-    const account = sut.add({
+    const account = await sut.add({
       name: "any_name",
       email: "any_email@mail.com",
       password: "any_password",
     });
+
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe("any_name");
