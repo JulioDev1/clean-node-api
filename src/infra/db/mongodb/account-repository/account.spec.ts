801afc493e40c298792cdc4bd16a28c7ac4ac54b
Julio Cesar <juliocesargualberto617@gmail.com>
2023-08-03 21:25:07 -0300

    test: add map function to mongo-helper

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -13,18 +13,24 @@ describe("Account Mongo Repository", () => {
     await MongoHelper.disconnect();
   });
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
+  });
+
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository();
   };
 
   test("Should return an account on sucess", async () => {
+    debugger;
     const sut = makeSut();
+
     const account = await sut.add({
       name: "any_name",
       email: "any_email@mail.com",
       password: "any_password",
     });
-
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe("any_name");
--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -9,7 +9,7 @@ export const MongoHelper = {
       throw new Error("enviroment ambient is not defined");
     }
 
-    this.client = await MongoClient.connect(MongoDB);
+    this.client = await MongoClient.connect(MongoDB, {});
   },
   async disconnect(): Promise<void> {
     await this.client.close();
@@ -17,5 +17,10 @@ export const MongoHelper = {
 
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
   },
 };
